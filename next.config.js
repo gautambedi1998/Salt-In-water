964bd9e94a67798f01bbb5d20cdb40b339def8ba
gautam bedi <41571537+gautambedi1998@users.noreply.github.com>
2022-04-09 15:53:02 +1000

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,37 @@
+const { withSentryConfig } = require('@sentry/nextjs')
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+// TODO: Remove dummy domains listed below once all images are migrated to siw domain
+const moduleExports = withBundleAnalyzer({
+  reactStrictMode: true,
+  experimental: {
+    esmExternals: false},
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'vercel.saleor.cloud',
+      process.env.CONTENTFUL_IMAGE_HOST,
+      process.env.COMMERCE_IMAGE_HOST,
+    ],
+  },
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+})
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+}
+
+// module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions)
+module.exports = moduleExports
